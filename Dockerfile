27a4adea961bfc3c7c79d6b8070e1e34192f2986
Victor Nabatov <victornabatov@gmail.com>
2017-12-24 19:05:11 +0300

    added binary and it works

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-FROM ethereum/client-go
 FROM node:boron
+
 
 # Create app directory
 WORKDIR /wileyriyumdocker
 
 COPY . .
-RUN npm install
-RUN pwd
+#RUN npm install
+#RUN pwd
 RUN ls -la
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
-apt-get update
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:ethereum/ethereum
-apt-get install -y ethereum
-geth --datadir /var/wileydao/blockchain/chaindata init wileyDAOGenesis.json
-nohup geth --datadir /var/wileydao/blockchain/chaindata --networkid 13 --nodiscover --rpc --rpcaddr "0.0.0.0" --rpcport 8545 --rpcapi "db,admin,debug,miner,shh,txpool,personal,eth,net,web3" --rpccorsdomain "*" > ./logs/gethnode1.log 2>&1 &
-nohup geth --datadir /var/wileydao/blockchain/chaindata --networkid 13 --nodiscover > ./logs/gethnode1.log 2>&1 &
-geth attach chaindata/geth.ipc
+./geth version
+#geth --datadir /var/wileydao/blockchain/chaindata init wileyDAOGenesis.json \
+#nohup geth --datadir /var/wileydao/blockchain/chaindata --networkid 13 --nodiscover --rpc --rpcaddr "0.0.0.0" --rpcport 8545 --rpcapi "db,admin,debug,miner,shh,txpool,personal,eth,net,web3" --rpccorsdomain "*" > ./logs/gethnode1.log 2>&1 & \
+#nohup geth --datadir /var/wileydao/blockchain/chaindata --networkid 13 --nodiscover > ./logs/gethnode1.log 2>&1 & \
+#geth attach chaindata/geth.ipc
