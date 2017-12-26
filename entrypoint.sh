d462e87919d5fa2f8f79f538e86f31cc0d17e450
Victor Nabatov <victornabatov@gmail.com>
2017-12-26 23:35:34 +0300

    import pk. start mining

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+privateKeyFile=$1
+password=$2
+email=$3
+
 if [ -f "/app/account" ]
 then
     # account found
@@ -8,19 +12,28 @@ else
     # init
     ./geth --datadir /app/data init ./wileyDAOGenesis.json
 
-    # generate account
-    echo $2 > password
+    # copy address
+    cp ./static-nodes.json /app/data/geth/
+
+    # import account
+    echo $password > password
+    ./geth --password ./password account import $privateKeyFile
     account=`./geth --datadir /app/data --password ./password account new | sed "s/[^{]\+{\([^}]\+\)}/\1/"`
     rm password
 
-    # copy account to the volume
-    echo $account > /app/account
+    #save account number
+    echo $account /app/account
+
+    # generate account
+    # echo $spassword > password
+    # account=`./geth --datadir /app/data --password ./password account new | sed "s/[^{]\+{\([^}]\+\)}/\1/"`
+    # rm password
 
     # send email
-    mailMessage="account:$account/password:$2"
-    npm run mail -- $1 $mailMessage
+    # mailMessage="privateKeyFile:$privateKeyFile/password:$password/account:$account"
+    # echo $mailMessage
+    # echo $email
+    # npm run mail -- $email $mailMessage
 fi
 
-#TODO: start mining here
-
-nohup geth --datadir /app/data --etherbase "0x$account" --mine --minerthreads=4
+./geth --datadir /app/data/ --networkid 13 --nodiscover --mine --minerthreads=8 --etherbase 0xdF22381674dED825462795a4d6fb5A4BeEA95739
--- stop.sh
+++ stop.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 container=`cat container`
-#TODO: save database to the volume
 docker stop $container
 docker rm $container
 rm container
